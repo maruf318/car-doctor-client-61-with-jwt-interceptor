a85c9e91ceca31edc7f2e0ec1884b4bceed901b6
Maruf Hossain <hossainmaruf001@gmail.com>
2023-10-26 01:08:51 +0200

    Insert bulk Data In Database and Load services data

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const ServiceCard = ({ service }) => {
-  const { title, img, price } = service || {};
+  const { _id, title, img, price } = service || {};
   return (
     <div className="card p-2 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
@@ -9,9 +11,11 @@ const ServiceCard = ({ service }) => {
         <h2 className="card-title text-2xl font-bold">{title}</h2>
         <p className="text-xl text-[#FF3811] font-semibold ">Price: ${price}</p>
         <div className="card-actions">
-          <button className="btn btn-outline btn-error text-white">
-            Buy Now
-          </button>
+          <Link to={`/checkout/${_id}`}>
+            <button className="btn btn-outline btn-error text-white">
+              Book Now
+            </button>
+          </Link>
         </div>
       </div>
     </div>
