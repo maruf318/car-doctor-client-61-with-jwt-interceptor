14638befbc8caa948372573df63256dfbe36da4c
Maruf Hossain <hossainmaruf001@gmail.com>
2023-10-26 16:07:47 +0200

    delete button and implementtation of a delete of a booking

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -15,6 +15,23 @@ const Bookings = () => {
         setBookings(data);
       });
   }, []);
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Deleted Successful");
+            const remaining = bookings.filter((booking) => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
   return (
     <div>
       <h2 className="text-5xl">Your Bookings: {bookings.length}</h2>
@@ -29,14 +46,20 @@ const Bookings = () => {
                 </label>
               </th>
               <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
             {bookings.map((booking) => (
-              <BookingRow key={booking._id} booking={booking}></BookingRow>
+              <BookingRow
+                key={booking._id}
+                booking={booking}
+                handleDelete={handleDelete}
+              ></BookingRow>
             ))}
           </tbody>
         </table>
