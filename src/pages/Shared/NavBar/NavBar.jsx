67a3ff75f3d87ade6baf117d3a49efb23f8c0a31
Maruf Hossain <hossainmaruf001@gmail.com>
2023-10-26 13:39:11 +0200

    created booking page to load booking of a current user

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -18,9 +18,14 @@ const NavBar = () => {
         <Link to={"/about"}>About</Link>
       </li>
       {user?.email ? (
-        <li>
-          <button onClick={handleLogOut}>Log Out</button>
-        </li>
+        <>
+          <li>
+            <Link to={"/bookings"}>My Bookings</Link>
+          </li>
+          <li>
+            <button onClick={handleLogOut}>Log Out</button>
+          </li>
+        </>
       ) : (
         <li>
           <Link to={"/login"}>Login</Link>
