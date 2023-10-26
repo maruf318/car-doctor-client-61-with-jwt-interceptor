b9d9069938237bb9acb86ff2f8f7dbb8b0300e66
Maruf Hossain <hossainmaruf001@gmail.com>
2023-10-26 14:18:06 +0200

    Display bookings and Service image in a tabular format

---
+++ src/pages/Bookings/BookingRow.jsx
@@ -0,0 +1,27 @@
+const BookingRow = ({ booking }) => {
+  const { date, service, price, img } = booking || {};
+  return (
+    <tr>
+      <th>
+        <label>
+          <input type="checkbox" className="checkbox" />
+        </label>
+      </th>
+      <td>
+        <div className="avatar">
+          <div className="w-24 h-24 rounded ">
+            {img && <img img src={img} alt="Avatar Tailwind CSS Component" />}
+          </div>
+        </div>
+      </td>
+      <td>{service}</td>
+      <td>{date}</td>
+      <td>${price}</td>
+      <th>
+        <button className="btn btn-ghost btn-xs">details</button>
+      </th>
+    </tr>
+  );
+};
+
+export default BookingRow;
